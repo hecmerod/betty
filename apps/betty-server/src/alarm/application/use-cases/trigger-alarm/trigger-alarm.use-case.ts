d4b33398c948ded72e3ed346a5b731d6e3ad91fd
hecemrod <hecmerod@gmail.com>
2026-06-08 16:56:37 +0200

    [Server] create alarm sound controller

--- apps/betty-server/src/alarm/application/use-cases/trigger-alarm/trigger-alarm.use-case.ts
+++ apps/betty-server/src/alarm/application/use-cases/trigger-alarm/trigger-alarm.use-case.ts
@@ -1,48 +1,75 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, OnModuleDestroy } from '@nestjs/common';
 import { AlarmRepository } from '../../../domain/repositories/alarm.repository';
 import { SendNotificationUseCase } from '../../../../notifications/application/use-cases/send-notification/send-notification.use-case';
 import { ALARM_REPOSITORY } from '../../../infrastructure/ioc/symbols';
+import { SetPinUseCase } from '../../../../gpio/application/use-cases/set-pin.use-case';
 
 @Injectable()
-export class TriggerAlarmUseCase {
+export class TriggerAlarmUseCase implements OnModuleDestroy{
   constructor(
     @Inject(ALARM_REPOSITORY)
     private readonly alarmRepository: AlarmRepository,
-    private readonly sendNotificationUseCase: SendNotificationUseCase
+    private readonly sendNotificationUseCase: SendNotificationUseCase,
+    private readonly setPinUseCase: SetPinUseCase,
   ) {}
 
+  private isAlarmSoundActive = false;
+  private alarmTimer = 0;
+
   async execute(
-    triggerData?: AlarmTriggerData
-  ): Promise<{ notificationSent?: boolean }> {
+    triggerData?: AlarmTriggerDataInput
+  ): Promise<boolean> {
     const alarm = await this.alarmRepository.get();
 
-    if (alarm.isActive) {
-      try {
-        await this.sendNotificationUseCase.execute({
-          notification: {
-            title: '🚨 BETTY ALARM',
-            body: 'Persona detectada en tu hogar',
-            imageUrl: '',
-          },
-          data: {
-            type: 'alarm',
-            timestamp: new Date().toISOString(),
-            detectionType: triggerData?.detectionType || 'person',
-            eventType: triggerData?.eventType || 'detection',
-          },
-        });
+    if (!alarm.isActive) return false;
 
-        return { notificationSent: true };
-      } catch {
-        return { notificationSent: false };
-      }
+    try {
+      await this.sendNotificationUseCase.execute({
+        notification: {
+          title: '🚨 BETTY ALARM',
+          body: 'Persona detectada en tu hogar',
+          imageUrl: '',
+        },
+        data: {
+          type: 'alarm',
+          timestamp: new Date().toISOString(),
+          detectionType: triggerData?.detectionType || 'person',
+          eventType: triggerData?.eventType || 'detection',
+        },
+      });
+
+      if(!this.isAlarmSoundActive) this.alarmSoundController(false);
+      else this.alarmTimer = 0;
+
+      return true;
+    } catch  {
+      return false;
     }
+  }
+
+  async alarmSoundController(state: boolean) {
+    this.isAlarmSoundActive = true;
+
+    await this.setPinUseCase.execute(17, state)
+
+    if(this.alarmTimer < 10) {
+      this.alarmTimer++;
+      setTimeout(() => this.alarmSoundController(!state), state ? 200 : 500);
+    }
+    
+    await this.setPinUseCase.execute(17, true);
+    this.isAlarmSoundActive = false;
+  }
+
+  async onModuleDestroy() {
+    await this.setPinUseCase.execute(17, true);
   }
 }
 
-export interface AlarmTriggerData {
+export interface AlarmTriggerDataInput {
   eventType: string;
   detectionType: string;
   confidence?: number;
   metadata?: Record<string, unknown>;
 }
+
